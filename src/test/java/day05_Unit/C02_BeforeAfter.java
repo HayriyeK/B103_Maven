fbfd7cc3e104abbe62e3a7444f3abf9c8a8e2778
Hayriye Kılıç <kilichayriye03@gmail.com>
2023-01-04 23:59:11 +0300

    04/01/2023

---
+++ src/test/java/day05_Unit/C02_BeforeAfter.java
@@ -0,0 +1,28 @@
+package day05_Unit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class C02_BeforeAfter {
+    @Before
+    public void setUp(){
+        System.out.println("Her Test Methodundan önce çalisir");
+    }
+    public void tearDown(){
+        System.out.println("her test methodundan sonra calışir");
+    }
+
+    @Test
+    public void test01(){
+        System.out.println("ilk test");
+
+    }
+    @Test
+    public void test02(){
+        System.out.println("ikinci test");
+
+    }
+
+
+}
