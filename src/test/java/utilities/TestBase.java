9c2e511d008f7f4c11bc29f7e3267962d0233d40
Hayriye Kılıç <kilichayriye03@gmail.com>
2023-01-06 23:52:09 +0300

    06/01/2023

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestBase {
 //     driver objesini olustur. Driver ya public yada protected olmali. Sebepi child classlarda gorulebilir olmasi
@@ -24,7 +26,9 @@ public class TestBase {
     }
 //    tearDown
     @After
-    public void tearDown() {
+    public void tearDown()
+    {
+        waitFor(5);
         driver.quit();
     }
 
@@ -44,5 +48,22 @@ public class TestBase {
             }
         }
         driver.switchTo().window(origin);
+    }
+    //    windowNumber sıfır (0)'dan başlıyor.
+//    index numarasini parametre olarak alir
+//    ve o indexli pencerece gecis yapar
+    public static void switchToWindow(int windowNumber){
+        List<String> list = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(list.get(windowNumber));
+    }
+    /*   HARD WAIT:
+ @param : second
+*/
+    public static void waitFor(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
