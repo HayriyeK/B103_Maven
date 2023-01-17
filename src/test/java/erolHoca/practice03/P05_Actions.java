7ef5ecc63fa648221affd15f0435f20c44328ebb
Hayriye Kılıç <kilichayriye03@gmail.com>
2023-01-17 23:57:58 +0300

    17/01/2023

--- src/test/java/erolHoca/practice03/P05_Actions.java
+++ src/test/java/erolHoca/practice03/P05_Actions.java
@@ -1,6 +1,9 @@
 package erolHoca.practice03;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class P05_Actions extends TestBase {
@@ -8,6 +11,21 @@ public class P05_Actions extends TestBase {
     public void name() {
         //  - http://szimek.github.io/signature_pad/ sayfasına gidiniz
         driver.get("http://szimek.github.io/signature_pad/");
+        WebElement yaziTahtasi = driver.findElement(By.xpath("//canvas"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(yaziTahtasi).clickAndHold();
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(5,5);
+        }
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(0,5);
+        }
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(5,0);
+        }
+        actions.release().build().perform();
+        driver.findElement(By.xpath("//*[text()='clear']")).click();
+
         //    - Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
 
         //    - Cizdikten sonra clear butonuna basalım
